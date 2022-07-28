61c78053aedacacc2578c8e78160ae5996355687
Souravdey777 <piyush.kolkata@gmail.com>
2022-07-29 01:36:09 +0530

    ✨ theme and styled components config done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './theme/index';
+import THEME_CONSTANT from './theme/constant';
 
 const App = () => {
-	return <h1>Welcome to Anarrativ</h1>;
-};
+	const [theme, setTheme] = useState('light');
 
+	const themeToggler = () => {
+		theme === 'light' ? setTheme('dark') : setTheme('light');
+	};
+
+	return (
+		<ThemeProvider theme={THEME_CONSTANT[theme]}>
+			<>
+				<GlobalStyles />
+				<div className="App">
+					<button onClick={themeToggler}>Switch Theme</button>
+					<h1>Welcome to Anarrativ</h1>
+					<p>
+						A sass platform to create and express your narrative in the form of
+						web stories. Add videos, pictures, links and share your awesome
+						story with the world.
+					</p>
+				</div>
+			</>
+		</ThemeProvider>
+	);
+};
 export default App;
---
+++ src/theme/constant.js
@@ -0,0 +1,16 @@
+const THEME_CONSTANT = {
+	light: {
+		body: '#FFF',
+		text: '#363537',
+		toggleBorder: '#FFF',
+		background: '#363537',
+	},
+	dark: {
+		body: '#363537',
+		text: '#FAFAFA',
+		toggleBorder: '#6B8096',
+		background: '#999',
+	},
+};
+
+export default THEME_CONSTANT;
